6c5f68b369ec75d8ed345e8f746bed142f43c833
kshinoz98 <kshinoz98@charlottesvilleschools.org>
2022-09-15 15:30:30 -0400

    working motor code

--- import board.py
+++ import board.py
@@ -1,8 +1,14 @@
 import board
 import time 
 import math
+import pwmio 
 from adafruit_motor import servo 
 
-myServo = servo.Servo()
+pwm = pwmio.PWMOut(board.D3, duty_cycle=2 **15, frequency=50)
+myServo = servo.Servo(pwm)
 
-myServo.angle = 90
+while True:
+    myServo.angle = 90 
+    time.sleep(1)
+    myServo.angle = 0
+    time.sleep(1)
