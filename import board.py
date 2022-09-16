e4a3acd7504d8867caea1f2c1c9e4b4c6c2f0502
kshinoz98 <kshinoz98@charlottesvilleschools.org>
2022-09-16 15:52:36 -0400

    Done rgb

--- import board.py
+++ import board.py
@@ -5,20 +5,18 @@ import adafruit_hcsr04
 import simpleio
 import neopixel
 dot=neopixel.NeoPixel(board.NEOPIXEL,1)
-Var = 0
-blue = 0
-green = 0
-red = 0
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
+dot.brightness=.1
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
 print("starting")
 while True:
     try:
-        print((sonar.distance,))
+        if sonar.distance < 5:
+            dot.fill((255,0,0))
+        elif sonar.distance < 10 and sonar.distance > 5:
+            dot.fill((simpleio.map_range(sonar.distance,5,10,255,0),0,simpleio.map_range(sonar.distance,5,10,0,255)))
+        else:
+            dot.fill((0,simpleio.map_range(sonar.distance,10,20,0,255),simpleio.map_range(sonar.distance,10,20,255,0)))
+        print((sonar.distance))
+        time.sleep(.001)
     except RuntimeError:
         print("Retrying!")
-    if sonar.distance > 0 and sonar.distance < 5:
-        dot.fill(simpleio.map_range(sonar.distance,0,5,255,0),0,simpleio.map_range(sonar.distance,0,5,0,255))
-    if sonar.distance > 5 and sonar.distance < 20:
-        dot.fill(0,simpleio.map_range(sonar.distance,0,5,0,255),simpleio.map_range(sonar.distance,0,5,255,0))
-    if sonar.distance > 20:
-        dot.fill(simpleio.map_range(sonar.distance,0,40,0,600))
